78dbb1db1a0f81e1fac8e1cf0518137369d6847a
smen851 <smen851@aucklanduni.ac.nz>
2022-03-11 18:27:43 +1300

    added snr integration with uniform sampling in doppler

--- patterncreator.py
+++ patterncreator.py
@@ -1,3 +1,5 @@
+from numpy import cos, sin
+
 from antenna import Pattern
 from farField import UniformAperture
 import numpy as np
@@ -10,8 +12,8 @@ filename = destination_folder+filename
 # %%
 # PATTERN GENERATION
 uniap = UniformAperture(4, .8)
-theta = np.linspace(0, np.pi / 2, 400)
-phi = np.linspace(0, np.pi / 2, 70)
+theta = np.linspace(0, np.pi / 16, 300)
+phi = np.linspace(0, np.pi * 2, 200)
 print('computing')
 Theta, Phi = np.meshgrid(theta, phi)
 g2 = uniap.mesh_gain_pattern(Theta, Phi).astype("complex")  # it underestimates, but just a bit
@@ -19,10 +21,29 @@ g2 = uniap.mesh_gain_pattern(Theta, Phi).astype("complex")  # it underestimates,
 print('done')
 
 # %% create the object
-pat = Pattern(theta, phi, g2)
+pat = Pattern(theta, phi, g2.T)
 
 # %% pickle the object at destination
 with open(filename, 'wb') as handle:
     pk.dump(pat, handle)
     handle.close()
     print('pickled')
+
+# %% unpickle and plot
+
+with open(filename, 'rb') as handle:
+    pat:Pattern = pk.load(handle)
+    handle.close()
+    print('unpickled')
+
+# %% plot pattern
+import matplotlib.pyplot as plt
+
+Theta, Phi = np.meshgrid(pat.theta_ax, pat.phi_ax)
+
+fig, ax  = plt.subplots(1)
+c = ax.pcolormesh(Theta * cos(Phi), Theta * sin(Phi) ,10*np.log10(np.abs(pat.gain_pattern.T)), cmap=plt.get_cmap('hot'))
+#c = ax.pcolormesh(Phi * cos(Theta), Phi * sin(Theta) ,10*np.log10(np.abs(pat.gain_pattern)), cmap=plt.get_cmap('hot'))
+fig.colorbar(c)
+ax.set_xlabel("$\\theta\  cos \phi$")
+ax.set_ylabel("$\\theta\  sin \phi$")
